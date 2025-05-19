78b83ef49b642a85aea0b3088a758439cce9ba48
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-05-19 10:10:44 -0400

    Testing git

--- MainCode.py
+++ MainCode.py
@@ -1,2 +1,22 @@
-print("Hello")
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+DIRECTORY = "."
+
+os.chdir(DIRECTORY)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    webbrowser.open(f"http://localhost:{PORT}/index.html")
+    httpd.serve_forever()
+
+
+
+
+
 
