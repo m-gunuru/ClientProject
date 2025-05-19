b286a4a8b9a5ff11fcaf421d1a20c94f29dcda9b
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-05-19 10:56:32 -0400

    Testing againnn

--- MainCode.py
+++ MainCode.py
@@ -9,11 +9,14 @@ DIRECTORY = "."
 os.chdir(DIRECTORY)
 
 Handler = http.server.SimpleHTTPRequestHandler
+html_file_path = "/Users/mohana/ClientProject/code/index.html"
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
+    webbrowser.open(html_file_path)
     webbrowser.open(f"http://localhost:{PORT}/index.html")
     httpd.serve_forever()
+    
 
 
 
