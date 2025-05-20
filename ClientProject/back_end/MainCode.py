110fa4189dca68a81414498e9765c74340f5eda8
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-05-20 10:45:55 -0400

    value

---
+++ ClientProject/back_end/MainCode.py
@@ -0,0 +1,25 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+DIRECTORY = "test"
+
+os.chdir(DIRECTORY)
+
+Handler = http.server.SimpleHTTPRequestHandler
+html_file_path = "/Users/mohana/ClientProject/code/index.html"
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    webbrowser.open(html_file_path)
+    webbrowser.open(f"http://localhost:{PORT}/index.html")
+    httpd.serve_forever()
+    
+# bgcolor = "#5385f1 "
+
+
+
+
+
