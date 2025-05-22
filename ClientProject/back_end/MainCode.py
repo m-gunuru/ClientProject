3f9af0bbc5f2284b60d6e5846c0668f4e6e24586
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-05-22 10:28:42 -0400

    saving local changes

--- ClientProject/back_end/MainCode.py
+++ ClientProject/back_end/MainCode.py
@@ -9,12 +9,12 @@ DIRECTORY = "../front_end"
 os.chdir(DIRECTORY)
 
 Handler = http.server.SimpleHTTPRequestHandler
-html_file_path = "/Users/mohana/ClientProject/code/ClientProject/front_end/index.html"
+html_file_path = "/Users/mohana/ClientProject/code/ClientProject/front_end/FrontPage.html"
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     webbrowser.open(html_file_path)
-    webbrowser.open(f"http://localhost:{PORT}/index.html")
+    webbrowser.open(f"http://localhost:{PORT}/FrontPage.html")
     httpd.serve_forever()
 
 
--- ClientProject/front_end/FrontPage.js
+++ ClientProject/front_end/FrontPage.js
@@ -1,1 +1,29 @@
-import React from 'react';
+
+// sign-up button
+const signB = document.createElement('button');
+signB.textContent = 'Sign Up';
+signB.className = 'signUpB';
+
+// when button is clicked a new page is loaded
+signB.addEventListener('click', function () {
+  window.location.href = 'SignInPage.html';
+});
+
+// append to container
+document.getElementById('button-container').appendChild(signB);
+
+
+//log-in textboxes
+const logUser = document.createElement('input');
+logUser.placeholder = 'Username';
+logUser.classList.add('log', 'logUser');
+
+
+// append to container
+document.getElementById('user-container').appendChild(logUser);
+
+
+const logPass = document.createElement('input');
+logPass.placeholder = 'Password';
+logPass.classList.add('log', 'logPass');
+document.getElementById('password-container').appendChild(logPass);
