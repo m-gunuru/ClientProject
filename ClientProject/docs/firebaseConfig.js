de0dce2794debc49c42bdaeacab5efcae6a0c0c2
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-05-30 10:04:15 -0400

    Renamed front_end to docs for link

---
+++ ClientProject/docs/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCD_MdnDepCDnRYQqxf2hUehpxBhR5_aBo",
+  authDomain: "clientproject-eacce.firebaseapp.com",
+  projectId: "clientproject-eacce",
+  storageBucket: "clientproject-eacce.firebasestorage.app",
+  messagingSenderId: "460906541362",
+  appId: "1:460906541362:web:b095c8fd7ae35486cea72b",
+  measurementId: "G-JPGW0P68M7"
+};
