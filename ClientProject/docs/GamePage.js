e72050b0a894724b909a9d67fba81fcd6e2a3918
Mohana Gunuru <mohana.gunuru@gmail.com>
2025-06-03 09:22:36 -0400

    Updating files

--- ClientProject/docs/GamePage.js
+++
@@ -1,9 +0,0 @@
-const start = document.createElement('button');
-start.textContent = 'Start';
-start.className = 'start';
-
-start.addEventListener('click', function() {
-    window.location.href = 'Dialogue.html';
-});
-
-document.getElementById('start-container').appendChild(start);
